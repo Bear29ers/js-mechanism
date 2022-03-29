31d3859be410b30515203cbeab2837e97eec3a0b
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-29 09:19:59 +0900

    実行コンテキスト

---
+++ 02_Execute_Environment/02_Execution_Context/main.js
@@ -0,0 +1,10 @@
+let a = 0;
+function b() {
+  console.log(arguments);  // argumentsが使用できる
+  console.log(this);  // thisを使用できる
+  console.log(a);  // 外部変数（関数bの外で宣言された変数）aを使用することができる
+  // ここではsuperは使用できない
+}
+
+console.log(a);
+b();
