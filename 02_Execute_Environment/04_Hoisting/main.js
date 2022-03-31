63cfe031b3530dc32bb57bee164e8416c03c4cd1
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-31 23:51:16 +0900

    ホイスティング

---
+++ 02_Execute_Environment/04_Hoisting/main.js
@@ -0,0 +1,35 @@
+// function（関数）宣言はホイスティングされる
+a();
+
+function a() {
+  console.log(e);
+  var e = 1;
+
+  f();
+  function f() {
+    console.log('f is called');
+  }
+
+  console.log('a is called');
+}
+
+// var宣言はホイスティングされる
+var b;
+console.log(b);
+
+b = 0;
+console.log(b);
+
+// let宣言はホイスティングされない
+console.log(c);
+let c = 0;
+
+// const宣言はホイスティングされない
+console.log(d);
+const d = 0;
+
+// 関数式はホイスティングされない
+g();
+const g = function() {
+  console.log('Hello World');
+}
