90ffe315188cbfe2c11ff2227da1281341e3aa34
Yuta Okuma <y.dream.nest@icloud.com>
2022-06-10 22:54:07 +0900

    コンストラクタ関数

---
+++ 06_Functions_Objects_2/01_Constructor_Functions/main.js
@@ -0,0 +1,9 @@
+/* コンストラクタ関数 */
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const bob = new Person('Bob', 18);
+const tom = new Person('Tom', 33);
+const jack = new Person('Jack', 20);
