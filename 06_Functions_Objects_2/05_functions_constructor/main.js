8cae6d6f75561bc3d54df42cab7ecbbba2e5f41b
Yuta Okuma <y.dream.nest@icloud.com>
2022-06-17 22:17:33 +0900

    関数コンストラクタ

---
+++ 06_Functions_Objects_2/05_functions_constructor/main.js
@@ -0,0 +1,28 @@
+/* 関数コンストラクタ */
+let d = 0;
+function fn() {
+  let d = 1;
+  const fn1 = new Function('a', 'b', 'return a * b * d');
+  return fn1;
+}
+
+const f = fn();
+const result = f(1, 2);
+console.log(result);
+
+function fn2(a, b) {
+  return a + b;
+}
+
+// fn2もFunctionコンストラクタから生成されているため、オブジェクトであるとわかる
+console.log(fn2 instanceof Function);
+
+// 関数からインスタンス化されたオブジェクト
+const obj = new function() {
+  this.a = 0;
+}
+
+// 関数コンストラクタで作成した関数
+const fn3 = new Function('this.a = 0;');
+const obj3 = new fn3();
+console.log(obj, obj3);
